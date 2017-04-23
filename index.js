be656d010873e85869f5331411d81b3acf3ac733
Richard Towers <richard_towers@yahoo.co.uk>
2017-04-23 19:24:19 +0100

    WIP: Insert zoopla data to the db on heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const pg = require('pg')
 const url = require('url')
+const request = require('request')
+const parseString = require('xml2js').parseString
 require('dotenv').config()
 
 const databaseUrl = process.env.DATABASE_URL
@@ -14,20 +16,34 @@ const connectionSettings = {
   ssl: true
 }
 
+const handleError = f => function (err) {
+  if (err) throw err
+  else if (f) f.apply(this, Array.from(arguments).splice(1))
+}
+
 const client = new pg.Client(connectionSettings)
 
-const handleError = err => { if (err) throw err }
-
-const handleQueryResult = (err, result) => {
-  handleError(err)
+const handleQueryResult = result => {
   console.log(result.rows)
-  client.end(handleError)
+  
 }
 
-const handleConnection = err => {
-  handleError(err)
-  client.query('select * from public.fruits', handleQueryResult)
-}
 
-client.connect(handleConnection)
+const args = process.argv
+const place = args[args.length-1]
+
+request('http://api.zoopla.co.uk/api/v1/property_listings.xml?postcode=' + place + '&page_size=100&include_sold=1&listing_status=sale&api_key=4psufpf7vmrvpngfrc3zuyqm',
+  (error, response, body) => {
+   parseString(body, (err, result) => {
+     client.query(
+       "insert into public.properties (outcode) values ('" + result.response.listing[0].outcode + "')",
+       handleError(() => client.end(handleError())))
+    })
+  })
+
+
+
+const handleConnection = () => { }
+
+client.connect(handleError(handleConnection))
 
